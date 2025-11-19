b0e309be3df7a6d6c15d0832bf413b053e5aa97d
jonbanks87 <jon.banks.87@gmail.com>
2025-11-19 15:24:36 -0700

    Stored password in environment variables. Added Longmont Site.

--- snotel/snotel_simple.py
+++ snotel/snotel_simple.py
@@ -1,7 +1,7 @@
+###### Imports ######
 import pandas as pd
 import requests
 import urllib3
-
 import sys
 import urllib3
 import numpy as np
@@ -13,11 +13,13 @@ import re
 import math
 import webbrowser
 import matplotlib as mpl
+from timezonefinder import TimezoneFinder
+import pytz
+from suntime import Sun
 
 tab10 = plt.get_cmap('tab10')
 
-### Run This
-
+##################### Functions #####################
 
 aspect_dict = {'N':0,
      'NNE':22.5,
@@ -60,7 +62,7 @@ def get_snotel_df(SiteName, years = 'recent'):
     StateAbb = site_df.loc[SiteName]['state']
     ############## Snotel
     today_string = datetime.today().date().strftime('%Y-%m-%d')
-    threedaysago_string = (datetime.today() - timedelta(days = 3)).date().strftime('%Y-%m-%d')
+    threedaysago_string = (datetime.today() - timedelta(days = 8)).date().strftime('%Y-%m-%d')
     if years == 'recent':
         StartDate = threedaysago_string
         EndDate = today_string
@@ -177,47 +179,40 @@ def get_nearest_snotel_site(name, lat, lon):
                       'snotel_sitenumber':sitenumber,
                       'state':state})
 
+def get_new_snow(snotel_df):
+    yesterday_string = (datetime.today() - timedelta(days = 1)).date().strftime('%Y-%m-%d')
+    snotel_df_24h = snotel_df[snotel_df['Date'] > yesterday_string]
 
+    new_snow_24h = snotel_df_24h[snotel_df_24h['new_snow'] >= 0]['new_snow'].sum()
+    return new_snow_24h
+
+def get_new_snow_and_df(SiteName):
+    snotel_df = get_snotel_df(SiteName)
+    new_snow_24h = get_new_snow(snotel_df)
+    return new_snow_24h
+
+############## Import Sites ##############
 
 snotel_sites_df = pd.read_csv('snotel_sites.csv') # From https://wcc.sc.egov.usda.gov/nwcc/yearcount?network=sntl&state=&counttype=statelist
 snotel_sites_df[['name','number']] = snotel_sites_df['site_name'].apply(lambda x: pd.Series(split_name_num(x)))
 snotel_sites_df.set_index('name', inplace= True)
 
 site_df = pd.read_csv('sites.csv')
-# site_df['snotel_sitename'] = site_df.apply(lambda x: get_nearest_snotel_site(x['name'],x['lat'],x['lon']), axis = 1)
 site_df[['snotel_sitename','snotel_sitenumber','state']] = site_df.apply(lambda x: get_nearest_snotel_site(x['name'],x['lat'],x['lon']), axis = 1)
 site_df.set_index('name', inplace = True)
 if int(site_df.index.value_counts().max()) > 1:
     raise ValueError('Duplicate Entries in sites.csv file. All names must be unique')
-# display(site_df)
 
-
-# SiteName = 'Bear Lake'
-
-for SiteName in ['Bear Lake']:
-
+########### Get Snotel Data and Plot ##############
+for SiteName in ['Bear Lake', 'Longmont']:
     SiteID = site_df.loc[SiteName]['snotel_sitenumber']
     human_snotel_url = f'https://wcc.sc.egov.usda.gov/nwcc/site?sitenum={SiteID}'
-
     lat = site_df.loc[SiteName]['lat']
     lon = site_df.loc[SiteName]['lon']
 
     co_sites_df = snotel_sites_df[snotel_sites_df['state'] == 'CO']
 
     snotel_df = get_snotel_df(SiteName)
-
-    def get_new_snow(snotel_df):
-        yesterday_string = (datetime.today() - timedelta(days = 1)).date().strftime('%Y-%m-%d')
-        snotel_df_24h = snotel_df[snotel_df['Date'] > yesterday_string]
-
-        new_snow_24h = snotel_df_24h[snotel_df_24h['new_snow'] >= 0]['new_snow'].sum()
-        return new_snow_24h
-
-    def get_new_snow_and_df(SiteName):
-        snotel_df = get_snotel_df(SiteName)
-        new_snow_24h = get_new_snow(snotel_df)
-        return new_snow_24h
-
     new_snow_24h = get_new_snow(snotel_df)
 
 
@@ -255,6 +250,7 @@ for SiteName in ['Bear Lake']:
     print('     Received snowfall and skycover data')
     snowfall_df = pd.json_normalize(grid_data_json['properties']['snowfallAmount']['values'])
     skycover_df = pd.json_normalize(grid_data_json['properties']['skyCover']['values'])
+    precip_prob_df = pd.json_normalize(grid_data_json['properties']['probabilityOfPrecipitation']['values'])
 
     print('     Parsing snowfall data')
     snowfall_df['startTime'] = snowfall_df['validTime'].apply(lambda x: parse_time_period2(x)[0])
@@ -263,6 +259,10 @@ for SiteName in ['Bear Lake']:
     print('     Parsing skycover data')
     skycover_df['startTime'] = skycover_df['validTime'].apply(lambda x: parse_time_period2(x)[0])
     skycover_df['endTime'] = skycover_df['validTime'].apply(lambda x: parse_time_period2(x)[1])
+
+    print('     Parsing precip prob data')
+    precip_prob_df['startTime'] = precip_prob_df['validTime'].apply(lambda x: parse_time_period2(x)[0])
+    precip_prob_df['endTime'] = precip_prob_df['validTime'].apply(lambda x: parse_time_period2(x)[1])
 
 
 
@@ -301,7 +301,42 @@ for SiteName in ['Bear Lake']:
     noaa_df['wind_y'] = noaa_df['windDirection'].apply(lambda x: direction_dict[x][1])
     noaa_df['total_hours'] = (noaa_df['startTime']-noaa_df['startTime'].min()).dt.total_seconds()/(60*60)
 
+    # Change timezones
 
+
+    tf = TimezoneFinder()
+    timezone_str = tf.timezone_at(lat=lat, lng=lon)
+    timezone = pytz.timezone(timezone_str)
+    dt = datetime.now()
+    timezone_diff = timezone.utcoffset(dt)
+
+    # timezone_diff = timedelta(hours = 6)
+    for df in [noaa_df,skycover_df, precip_prob_df]:
+        df['startTime_utc'] = df['startTime']
+        df['endTime_utc'] = df['endTime']
+        df['startTime_local'] = df['startTime_utc'] + timezone_diff
+        df['endTime_local'] = df['endTime_utc'] + timezone_diff
+
+    #Interpolating precip prob df
+    precip_prob_df_simple = pd.DataFrame(columns = ['time_local','value'])
+    for index, line in precip_prob_df.iterrows():
+        current_time = line['startTime_local']
+        end_time = line['endTime_local']
+        while current_time <= end_time:
+            precip_prob_df_simple.loc[len(precip_prob_df_simple)] = [current_time, line['value']]
+            current_time += timedelta(hours = 1)
+
+    ### Sunrise and Sunset ####
+    sun_df = pd.DataFrame(columns = ['date','sunrise','sunset','next_sunrise'])
+    for date in noaa_df.startTime_local.dt.date.unique():
+        sun = Sun(lat, lon)
+        
+        sunrise_time = (sun.get_local_sunrise_time(date) + timezone_diff).time()
+        sunrise = datetime.combine(date,sunrise_time)
+        sunset_time = (sun.get_local_sunset_time(date) + timezone_diff).time()
+        sunset = datetime.combine(date,sunset_time)
+        next_sunrise = sun.get_local_sunrise_time(date + timedelta(days = 1)) + timezone_diff
+        sun_df.loc[len(sun_df)] = [date, sunrise, sunset, next_sunrise]
 
     ########### Plotting ##################
     print("plotting")
@@ -309,24 +344,28 @@ for SiteName in ['Bear Lake']:
     fig.set_figwidth(10)
     fig.set_figheight(7)
 
-    min_time = noaa_df['startTime'].min()
-    max_time = noaa_df['startTime'].max()
+    min_time = noaa_df['startTime_local'].min()
+    max_time = noaa_df['startTime_local'].max()
 
     temp_norm = plt.Normalize(22, 42)
 
-    temp_plot = noaa_df.plot.scatter(x = 'startTime', y = 'temperature', ax = ax[0], xlim = (min_time, max_time), c = 'temperature', cmap = 'coolwarm', norm = temp_norm)
+    temp_plot = noaa_df.plot.scatter(x = 'startTime_local', y = 'temperature', ax = ax[0], xlim = (min_time, max_time), c = 'temperature', cmap = 'coolwarm', norm = temp_norm)
     ax[0].collections[0].colorbar.remove()
     ax[0].set_ylabel('Temperature (F)')
+    ax[0].scatter(x = sun_df.sunrise, y = [noaa_df['temperature'].min()-5 for n in range(len(sun_df))], marker='$\u263C$', c = 'gold')
+    ax[0].scatter(x = sun_df.sunset, y = [noaa_df['temperature'].min()-5 for n in range(len(sun_df))], marker='$\u263E$')
+
+
+
     snowfall_norm = plt.Normalize(0,0.5)
-    noaa_df.plot.scatter(x = 'startTime', y = 'total_snowfall_in', ax = ax[1], xlim = (min_time, max_time), c = 'snowfall_in', norm = snowfall_norm, cmap = 'viridis')
+    noaa_df.plot.scatter(x = 'startTime_local', y = 'total_snowfall_in', ax = ax[1], xlim = (min_time, max_time), c = 'snowfall_in', norm = snowfall_norm, cmap = 'viridis')
     ax[1].collections[0].colorbar.remove()
     ax[1].set_ylabel('Snowfall (in)')
 
     ax11 = ax[1].twinx()
-    noaa_df.plot(x = 'startTime', y = 'snowfall_in', ax = ax11, xlim = (min_time, max_time), c = tab10(1), x_compat=True, alpha = 0.3, legend = False, label = 'Snowfall Rate')
-    # noaa_df.plot.bar(x = 'startTime', y = 'snowfall_in', ax = ax11, xlim = (min_time, max_time))
+    noaa_df.plot(x = 'startTime_local', y = 'snowfall_in', ax = ax11, xlim = (min_time, max_time), c = tab10(1), x_compat=True, alpha = 0.3, legend = False, label = 'Snowfall Rate')
     ax11.set_ylabel('Snowfall Rate (in/hour)')
-    ax11.fill_between(noaa_df['startTime'], noaa_df['snowfall_in'], 0, color=tab10(1), alpha=0.3)
+    ax11.fill_between(noaa_df['startTime_local'], noaa_df['snowfall_in'], 0, color=tab10(1), alpha=0.3)
     from labellines import labelLine, labelLines
     labelLines(ax11.get_lines(), zorder=2.5, align = True, xvals = [0.5], yoffsets=[0.15])
 
@@ -335,13 +374,13 @@ for SiteName in ['Bear Lake']:
         axis.grid(which='major', color='#DDDDDD', linewidth=0.8)
         axis.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5)
 
-    skycover_df.plot(x = 'startTime', y = 'value', ax = ax[2], label = 'Skycover', c = '#a3a3a3', x_compat = True, legend = False)
-    ax[2].fill_between(skycover_df['startTime'], skycover_df['value'], 0, color='#a3a3a3', alpha=0.3)
-    ax[2].fill_between(skycover_df['startTime'], skycover_df['value'], 100, color='#6bc9ff', alpha=0.3)
+    skycover_df.plot(x = 'startTime_local', y = 'value', ax = ax[2], label = 'Skycover', c = '#a3a3a3', x_compat = True, legend = False)
+    ax[2].fill_between(skycover_df['startTime_local'], skycover_df['value'], 0, color='#a3a3a3', alpha=0.3)
+    ax[2].fill_between(skycover_df['startTime_local'], skycover_df['value'], 100, color='#6bc9ff', alpha=0.3)
     ax[2].set_ylabel('Skycover (%)')
 
     wind_norm = plt.Normalize(0,30)
-    idx = mpl.dates.date2num(noaa_df['startTime'])
+    idx = mpl.dates.date2num(noaa_df['startTime_local'])
     ax[3].quiver(idx, noaa_df['windSpeed_mph'],noaa_df['wind_x'],noaa_df['wind_y'],noaa_df['windSpeed_mph'], norm = wind_norm)
     ax[3].set_ylabel('Wind Speed (mph)')
     ax[3].set_ylim(0,noaa_df['windSpeed_mph'].max()+ 5)
@@ -349,6 +388,19 @@ for SiteName in ['Bear Lake']:
     date_format = mdates.DateFormatter("%a, %m-%d")
     ax[3].xaxis.set_major_formatter(date_format)
 
+    ax22 = ax[2].twinx()
+    # precip_prob_df_simple.plot(x = 'time_local', y = 'value', ax = ax[4], label = 'probabilityOfPrecipitation', c = '#a3a3a3', x_compat = True, legend = False)
+    ax22.fill_between(precip_prob_df_simple['time_local'], precip_prob_df_simple['value'], 0, color='none', alpha=0.3, hatch = 'OO', edgecolor='#005791')
+    # ax[4].fill_between(precip_prob_df_simple['time_local'], precip_prob_df_simple['value'], 100, color='#6bc9ff', alpha=0.3)
+    ax22.set_ylabel('Precip (%)')
+    ax22.set_ylim(0,100)
+    labelLines(ax22.get_lines(), zorder=2.5, align = True, xvals = [0.5], yoffsets=[0.15])
+
+    for index, row in sun_df.iterrows():
+        sunset = row['sunset']
+        next_sunrise = row['next_sunrise']
+        for axis in ax:
+            axis.axvspan(sunset, next_sunrise, facecolor='gray', alpha=0.07)
 
     fig2, ax2 = plt.subplots()
     fig2.suptitle(f'Snotel Report for {SiteName}')
@@ -360,10 +412,11 @@ for SiteName in ['Bear Lake']:
     ax2.set_ylabel('Snow Depth (in)')
     fig2.tight_layout()
 
+
     today = datetime.today()
     tomorrow = datetime.today() + timedelta(days = 1)
     two_days = datetime.today() + timedelta(days = 2)
-    noaa_df_24h = noaa_df[noaa_df['startTime'].between(today,tomorrow)]
+    noaa_df_24h = noaa_df[noaa_df['startTime_local'].between(today,tomorrow)]
     high_temp = noaa_df_24h['temperature'].max()
     low_temp = noaa_df_24h['temperature'].min()
     average_windspeed = round(noaa_df_24h['windSpeed_mph'].mean())
@@ -381,12 +434,12 @@ for SiteName in ['Bear Lake']:
     fig.set_tight_layout('tight')
     fig.subplots_adjust(hspace=0)
     plt.grid(True)
-    # date_format = mdates.DateFormatter('%Y-%m-%d')  # Specify the desired format
-    # ax2.xaxis.set_major_formatter(date_format)
-
+    date_format = mdates.DateFormatter('%Y-%m-%d')  # Specify the desired format
 
     caic_url = f'https://avalanche.state.co.us/?lat={lat}&lng={lon}'
-    # webbrowser.open(caic_url)
+
+
+    ########### Unique to Snotel_Simple.py ###########
     fig.savefig('daily_forecast.png',dpi = 300)
     fig2.savefig('daily_snotel_report.png',dpi = 300)
 
@@ -400,9 +453,16 @@ for SiteName in ['Bear Lake']:
     from email import encoders
     import smtplib
     import ssl
+    import os
 
+    
     email_sender = 'snowbanks.weather@gmail.com'
-    email_password = 'bluzwkxzcxfzgnok'
+    # Password is stored in environment variable for security
+    # To set the environment variable, in CmdPrompt use:
+    # set snowbanks_password=yourpasswordhere
+    # In the google account, this seems to need to be an App Password, not the regular account password
+    # I couldn't navigate to this, but if I went to myaccount.google.com, I searched for "App Passwords" and it took me to the right place
+    email_password = os.getenv('snowbanks_password')
     email_receiver = 'jon.banks.87@gmail.com'
 
     def send_emails(email_list):
@@ -420,7 +480,7 @@ for SiteName in ['Bear Lake']:
             msg['From'] = email_sender
             msg['To'] = person
             date = datetime.today().strftime('%Y-%m-%d')
-            msg['Subject'] = f'Daily Snow Report for {SiteName} - {date}'
+            msg['Subject'] = f'{SiteName} Daily Snow Report for {date}'
             msg.attach(MIMEText(body, 'plain'))
 
             for filename in ['daily_forecast.png', 'daily_snotel_report.png']:
@@ -449,3 +509,4 @@ for SiteName in ['Bear Lake']:
         TIE_server.quit()
 
     send_emails([email_receiver])
+    # plt.show()
